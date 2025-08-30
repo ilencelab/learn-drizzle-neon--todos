3759d4c853aa20cfd85c04bf6ef13bdf188f2fea
ilenceye <ye66169@gmail.com>
2025-08-31 07:38:42 +0800

    chore: remove unnecessary code

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,14 +1,10 @@
-import { AuthProvider } from "@/contexts/auth";
-import { getCurrentUser } from "@/lib/auth";
 import Link from "next/link";
 
 const routes = ["todos", "movies"];
 
 export default async function Page() {
-  const user = await getCurrentUser();
-
   return (
-    <AuthProvider userId={user.id}>
+    <>
       <div className="px-4 py-8">
         <ul className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
           {routes.map((r) => (
@@ -18,6 +14,6 @@ export default async function Page() {
           ))}
         </ul>
       </div>
-    </AuthProvider>
+    </>
   );
 }
